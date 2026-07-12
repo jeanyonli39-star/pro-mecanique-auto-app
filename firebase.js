855bd71e61b082531e03222da07cf2f74a05d122
jeanyonli39-star <jeanyonli39@gmail.com>
2026-07-12 19:18:17 +0000

    Initialize Firebase with configuration settings
    valider la modification avec succès

--- firebase.js
+++ firebase.js
@@ -1,1 +1,15 @@
+// Import Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDBtZTROKkGWZ1WeX3KLXvyXe48oS1B3zg",
+  authDomain: "promecaniqueauto.firebaseapp.com",
+  projectId: "promecaniqueauto",
+  storageBucket: "promecaniqueauto.firebasestorage.app",
+  messagingSenderId: "951329003648",
+  appId: "1:951329003648:web:cf3c514a2a8823b28eee65"
+};
+
+const app = initializeApp(firebaseConfig);
+
+console.log("Firebase connecté !");
