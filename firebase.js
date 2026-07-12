a9a5ab52c16ebf03a67fb1316da4b031e8df857f
jeanyonli39-star <jeanyonli39@gmail.com>
2026-07-12 20:02:50 +0000

    Update firebase.js
    ok

--- firebase.js
+++ firebase.js
@@ -4,8 +4,9 @@ import {
   getAuth,
   createUserWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-const firebaseConfig = { const app = initializeApp(firebaseConfig); 
-const auth = getAuth(app);
+
+// Configuration Firebase
+const firebaseConfig = {
   apiKey: "AIzaSyDBtZTROKkGWZ1WeX3KLXvyXe48oS1B3zg",
   authDomain: "promecaniqueauto.firebaseapp.com",
   projectId: "promecaniqueauto",
@@ -14,6 +15,13 @@ const auth = getAuth(app);
   appId: "1:951329003648:web:cf3c514a2a8823b28eee65"
 };
 
+// Initialiser Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialiser Firebase Authentication
+const auth = getAuth(app);
+
+// Exporter pour les autres fichiers
+export { auth, createUserWithEmailAndPassword };
 
 console.log("Firebase connecté !");
