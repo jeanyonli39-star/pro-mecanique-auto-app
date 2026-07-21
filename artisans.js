bdf155773caac77d0ee7bf88ef0d4503e4d9b580
jeanyonli39-star <jeanyonli39@gmail.com>
2026-07-21 19:09:36 +0000

    Update artisans.js
    ok

--- artisans.js
+++ artisans.js
@@ -1,1 +1,23 @@
+// ===============================
+// ARTISANS - PRO MECANIQUE AUTO
+// Partie 1
+// ===============================
 
+import { db } from "./firebase.js";
+
+import {
+  collection,
+  query,
+  where,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const params = new URLSearchParams(window.location.search);
+const metier = params.get("metier");
+
+const titre = document.getElementById("titreMetier");
+const message = document.getElementById("message");
+
+if (metier) {
+  titre.textContent = metier;
+  }
