60ac503eac7efc7ef8c0fb8e53af10a72b751699
jeanyonli39-star <jeanyonli39@gmail.com>
2026-07-21 19:13:43 +0000

    Update artisans.js
    oui

--- artisans.js
+++ artisans.js
@@ -21,3 +21,45 @@ const message = document.getElementById("message");
 if (metier) {
   titre.textContent = metier;
   }
+// ===============================
+// Partie 2 : Affichage des artisans
+// ===============================
+
+async function chargerArtisans() {
+
+  if (!metier) return;
+
+  const q = query(
+    collection(db, "artisans"),
+    where("metier", "==", metier)
+  );
+
+  const resultat = await getDocs(q);
+
+  if (resultat.empty) {
+    message.textContent =
+      "Aucun artisan inscrit dans cette catégorie pour le moment.";
+    return;
+  }
+
+  message.innerHTML = "";
+
+  resultat.forEach((doc) => {
+
+    const artisan = doc.data();
+
+    message.innerHTML += `
+      <div class="vente-box" style="margin-bottom:20px;">
+        <h3>${artisan.nom}</h3>
+        <p><strong>Métier :</strong> ${artisan.metier}</p>
+        <p><strong>Ville :</strong> ${artisan.ville}</p>
+        <p><strong>Téléphone :</strong> ${artisan.telephone}</p>
+        <p><strong>Description :</strong> ${artisan.description}</p>
+      </div>
+    `;
+
+  });
+
+}
+
+chargerArtisans();
